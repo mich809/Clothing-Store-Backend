f4eabaa7fd810d32662e8e53c1e613e930739321
Michael Caridad <caridadmichael@gmail.com>
2022-11-21 17:13:31 -0400

    new file for user entity

---
+++ src/main/java/com/CaridadMichael/ClothingStore/model/AppUser.java
@@ -0,0 +1,71 @@
+package com.CaridadMichael.ClothingStore.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class AppUser {
+	@Id
+	private long id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private long telephone;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifiedAt;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(long telephone) {
+		this.telephone = telephone;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getModifiedAt() {
+		return modifiedAt;
+	}
+	public void setModifiedAt(LocalDateTime modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+	
+
+}
