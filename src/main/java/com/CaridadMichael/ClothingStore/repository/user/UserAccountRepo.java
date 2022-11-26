392448fffed5348e4530f01c21341e9c9af3a7ab
Michael Caridad <caridadmichael@gmail.com>
2022-11-25 20:25:33 -0400

    added repository annotation

--- src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAccountRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAccountRepo.java
@@ -1,9 +1,12 @@
 package com.CaridadMichael.ClothingStore.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.CaridadMichael.ClothingStore.model.user.UserAccount;
 
+
+@Repository
 public interface UserAccountRepo extends JpaRepository<UserAccount,Long> {
 
 }
