b7c2cd1bf8fc15fa511d89bc4f777a22b8ad0441
Michael Caridad <caridadmichael@gmail.com>
2022-11-28 19:15:13 -0400

    added new query method to add user address

--- src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAccountRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAccountRepo.java
@@ -1,14 +1,22 @@
 package com.CaridadMichael.ClothingStore.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.CaridadMichael.ClothingStore.model.user.UserAccount;
-
+import com.CaridadMichael.ClothingStore.model.user.UserAddress;
 
 @Repository
-public interface UserAccountRepo extends JpaRepository<UserAccount,Long> {
+public interface UserAccountRepo extends JpaRepository<UserAccount, Long> {
 	boolean existsByEmail(String email);
+
 	public UserAccount findByEmail(String email);
+
+	@Modifying
+	@Query("update UserAccount u set u.UserAddress = :UserAddress  where u.email = :email")
+	void updateAddress(@Param(value = "email") String email, @Param(value = "UserAddress") UserAddress userAddress);
 
 }
