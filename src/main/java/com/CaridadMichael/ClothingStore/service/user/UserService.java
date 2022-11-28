156c58f9db42b3bed91838cf22adbc2e3000123e
Michael Caridad <caridadmichael@gmail.com>
2022-11-28 13:37:13 -0400

    format code

--- src/main/java/com/CaridadMichael/ClothingStore/service/user/UserService.java
+++ src/main/java/com/CaridadMichael/ClothingStore/service/user/UserService.java
@@ -1,6 +1,4 @@
- package com.CaridadMichael.ClothingStore.service.user;
-
-
+package com.CaridadMichael.ClothingStore.service.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,32 +9,27 @@ import com.CaridadMichael.ClothingStore.repository.user.UserAccountRepo;
 
 @Service
 public class UserService {
-	
-		@Autowired
-		private UserAccountRepo userAccountRepo;
-	
-	
 
-	    public String createUser(String email , String password) {
-	    	if(userExists(email)) {
-	    		return "User" + email+ " already  exist";
-	    		
-	    	}
-	        UserAccount user = new UserAccount(email, password);	
-	        userAccountRepo.save(user);
-	        return "User " + email + " created";
-	       
-	    }
+	@Autowired
+	private UserAccountRepo userAccountRepo;
 
-	    public UserAccount getUser(String email){	       
-			return userAccountRepo.findByEmail(email);
-	    }
+	public String createUser(String email, String password) {
+		if (userExists(email)) {
+			return "User" + email + " already  exist";
 
-	    private boolean userExists(String email) {	        
-	        return userAccountRepo.existsByEmail(email);
-	    }
-	
-	
-	
+		}
+		UserAccount user = new UserAccount(email, password);
+		userAccountRepo.save(user);
+		return "User " + email + " created";
+
+	}
+
+	public UserAccount getUser(String email) {
+		return userAccountRepo.findByEmail(email);
+	}
+
+	private boolean userExists(String email) {
+		return userAccountRepo.existsByEmail(email);
+	}
 
 }
