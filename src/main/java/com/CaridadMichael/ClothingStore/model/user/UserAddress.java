e1029586ee1a9956b8a8ed78988ba339865b8e66
Michael Caridad <caridadmichael@gmail.com>
2022-12-02 22:08:57 -0400

    added one-to-one relationship with userAccount.

--- src/main/java/com/CaridadMichael/ClothingStore/model/user/UserAddress.java
+++ src/main/java/com/CaridadMichael/ClothingStore/model/user/UserAddress.java
@@ -6,93 +6,103 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 @Entity
 public class UserAddress {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;	
-	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
 	private String firstName;
-	private String lastName;	
+	private String lastName;
 	private String addressLineOne;
 	private String addressLineTwo;
 	private String city;
 	private String postalCode;
 	private String country;
 	private String telephone;
-	
 
-	@OneToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name="user_id")
-    private UserAccount userAccount;
+	@OneToOne
+    @MapsId
+	private UserAccount userAccount;
 
-	
-	
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public String getAddressLineOne() {
 		return addressLineOne;
 	}
+
 	public void setAddressLineOne(String addressLineOne) {
 		this.addressLineOne = addressLineOne;
 	}
+
 	public String getAddressLineTwo() {
 		return addressLineTwo;
 	}
+
 	public void setAddressLineTwo(String addressLineTwo) {
 		this.addressLineTwo = addressLineTwo;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getPostalCode() {
 		return postalCode;
 	}
+
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public void setUserAccount(UserAccount userAccount) {
+		this.userAccount = userAccount;
+
 	}
 
 }
