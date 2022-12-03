b9009eab6ff9f8d037e8ee9d47da5f50c7dda863
Michael Caridad <caridadmichael@gmail.com>
2022-12-02 22:09:46 -0400

    added getters/setters

--- src/main/java/com/CaridadMichael/ClothingStore/model/user/UserPayment.java
+++ src/main/java/com/CaridadMichael/ClothingStore/model/user/UserPayment.java
@@ -1,30 +1,72 @@
 package com.CaridadMichael.ClothingStore.model.user;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class UserPayment {
 	@Id
 	private long id;
-	
+
 	private String paymentType;
 	private String provider;
 	private String expireDate;
 	private String cardNumber;
-	
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)	
-	@OnDelete(action = OnDeleteAction.CASCADE)
-	@JsonIgnore
+
+	@OneToOne
 	@MapsId
-    private UserAccount userAccount;
+	private UserAccount userAccount;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public String getExpireDate() {
+		return expireDate;
+	}
+
+	public void setExpireDate(String expireDate) {
+		this.expireDate = expireDate;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
+
+	public void setUserAccount(UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
 
 }
