75e65b436e59ec65075d7faecd6283e633b7e12f
Michael Caridad <caridadmichael@gmail.com>
2022-12-02 22:08:16 -0400

    added one-to-one relationship with address/payment

--- src/main/java/com/CaridadMichael/ClothingStore/model/user/UserAccount.java
+++ src/main/java/com/CaridadMichael/ClothingStore/model/user/UserAccount.java
@@ -1,41 +1,64 @@
 package com.CaridadMichael.ClothingStore.model.user;
 
-
-
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
 public class UserAccount {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	
-	@Column(unique=true)
-	private String email;
-	
-	private String password;	
-	
-	@OneToOne(mappedBy="userAccount", fetch = FetchType.LAZY)
-    private UserAddress userAddress;	
 
-	public UserAccount() {}
-	
-	public UserAccount(String email , String password) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(unique = true)
+	private String email;
+
+	private String password;
+
+	@OneToOne(mappedBy = "userAccount", cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
+	private UserAddress address;
+
+	@OneToOne(mappedBy = "userAccount", cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
+	private UserPayment userPayment;
+
+	public UserAccount() {
+	}
+
+	public UserAccount(String email, String password) {
 		super();
 		this.setEmail(email);
 		this.password = password;
-		
+
 	}
+
+	public UserPayment getUserPayment() {
+		return userPayment;
+	}
+
+	public void setUserPayment(UserPayment userPayment) {
+		this.userPayment = userPayment;
+	}
+
+	public UserAddress getAddress() {
+		return address;
+	}
+
+	public void setAddress(UserAddress address) {
+		this.address = address;
+	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
@@ -43,6 +66,7 @@ public class UserAccount {
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -53,15 +77,14 @@ public class UserAccount {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}	
-	
-	public void setUserAddress(UserAddress userAddress) {
-		this.userAddress = userAddress;
 	}
-	
-	public UserAddress getUserAddress() {
-		return this.userAddress;
-	}
-	
+
+//	public void setUserAddress(UserAddress userAddress) {
+//		this.userAddress = userAddress;
+//	}
+//	
+//	public UserAddress getUserAddress() {
+//		return this.userAddress;
+//	}
 
 }
