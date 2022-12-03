fc83c1ff569753cc20babd063aaa616e3b20b896
Michael Caridad <caridadmichael@gmail.com>
2022-12-03 19:37:54 -0400

    add methods to handle add/remove payment and delete user.

--- src/main/java/com/CaridadMichael/ClothingStore/service/user/UserService.java
+++ src/main/java/com/CaridadMichael/ClothingStore/service/user/UserService.java
@@ -8,21 +8,24 @@ import org.springframework.stereotype.Service;
 
 import com.CaridadMichael.ClothingStore.model.user.UserAccount;
 import com.CaridadMichael.ClothingStore.model.user.UserAddress;
+import com.CaridadMichael.ClothingStore.model.user.UserPayment;
 import com.CaridadMichael.ClothingStore.repository.user.UserAccountRepo;
 import com.CaridadMichael.ClothingStore.repository.user.UserAddressRepo;
+import com.CaridadMichael.ClothingStore.repository.user.UserPaymentRepo;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserAccountRepo userAccountRepo;
-	
+
 	@Autowired
 	private UserAddressRepo userAddressRepo;
 
-	
+	@Autowired
+	private UserPaymentRepo userPaymentRepo;
 
-	public String createUser(String email, String password) {
+	public String createUserAccount(String email, String password) {
 		if (userAccountExists(email)) {
 			return "User" + email + " already  exist";
 
@@ -33,27 +36,41 @@ public class UserService {
 
 	}
 
+	public String deleteUserAccount(String email) {
+		userAccountRepo.deleteByEmail(email);
+		return "User " + email + " deleted";
+
+	}
+
 	public UserAccount getUserAccount(String email) {
 		return userAccountRepo.findByEmail(email);
+	}
+
+	public void addUserAddress(String email, UserAddress userAddress) {
+
+		UserAccount userAccount = getUserAccount(email);
+		userAccount.setAddress(userAddress);
+		userAddress.setUserAccount(userAccount);
+		userAccountRepo.save(userAccount);
+
+	}
+
+	public void deleteUserAddress(Long id) {
+		userAddressRepo.deleteById(id);
+		
+
+	}
+	public Optional<UserAddress> getUserAddress(Long id) {
+
+		return Optional.ofNullable(userAddressRepo.findById(id).orElseThrow(null));
+	}
+
+	public Optional<UserPayment> getUserPayment(Long id) {
+
+		return Optional.ofNullable(userPaymentRepo.findById(id).orElseThrow(null));
 	}
 
 	private boolean userAccountExists(String email) {
 		return userAccountRepo.existsByEmail(email);
 	}
-
-	public void addUserAddress(String email, UserAddress userAddress) {
-		
-		UserAccount userAccount = getUserAccount(email);
-		userAccount.setAddress(userAddress);
-		userAddress.setUserAccount(userAccount);
-		userAccountRepo.save(userAccount);
-		
-		
-	}
-
-	public Optional<UserAddress> getUserAddress(Long id) {
-		
-		return userAddressRepo.findById(id);
-	}
-
 }
