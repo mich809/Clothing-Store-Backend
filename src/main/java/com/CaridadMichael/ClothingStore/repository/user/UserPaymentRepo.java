72c3d46b27e4e09f6e0198bde0a6ea92673dd3d6
Michael Caridad <caridadmichael@gmail.com>
2022-12-02 22:10:01 -0400

    extend jpaRepository interface

--- src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserPaymentRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserPaymentRepo.java
@@ -1,5 +1,11 @@
 package com.CaridadMichael.ClothingStore.repository.user;
 
-public interface UserPaymentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.CaridadMichael.ClothingStore.model.user.UserPayment;
+
+@Repository
+public interface UserPaymentRepo extends JpaRepository<UserPayment, Long> {
 
 }
