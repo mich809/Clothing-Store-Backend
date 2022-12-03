89402316715221bf59a22acb632f2ab70db12b70
Michael Caridad <caridadmichael@gmail.com>
2022-12-02 22:09:13 -0400

    add jpaRepo extension

--- src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAddressRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAddressRepo.java
@@ -1,5 +1,11 @@
 package com.CaridadMichael.ClothingStore.repository.user;
 
-public interface UserAddressRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.CaridadMichael.ClothingStore.model.user.UserAddress;
+
+@Repository
+public interface UserAddressRepo extends JpaRepository<UserAddress, Long> {
 
 }
