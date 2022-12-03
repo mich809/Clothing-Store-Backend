64682effefd1f4d68a045bdca0aede2841fabfb0
Michael Caridad <caridadmichael@gmail.com>
2022-12-03 19:37:12 -0400

    added deleteByEmail method

--- src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAccountRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/user/UserAccountRepo.java
@@ -1,13 +1,9 @@
 package com.CaridadMichael.ClothingStore.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.CaridadMichael.ClothingStore.model.user.UserAccount;
-import com.CaridadMichael.ClothingStore.model.user.UserAddress;
 
 @Repository
 public interface UserAccountRepo extends JpaRepository<UserAccount, Long> {
@@ -15,6 +11,6 @@ public interface UserAccountRepo extends JpaRepository<UserAccount, Long> {
 
 	public UserAccount findByEmail(String email);
 
-	
+	void deleteByEmail(String email);
 
 }
