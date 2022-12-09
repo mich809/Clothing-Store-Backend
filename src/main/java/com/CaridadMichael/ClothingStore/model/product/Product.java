1b3d5530b3a7deb81c87469cd95f8000bde9aec8
Michael Caridad <caridadmichael@gmail.com>
2022-12-09 17:31:20 -0400

    nothing major

--- src/main/java/com/CaridadMichael/ClothingStore/model/product/Product.java
+++ src/main/java/com/CaridadMichael/ClothingStore/model/product/Product.java
@@ -2,8 +2,13 @@ package com.CaridadMichael.ClothingStore.model.product;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+import com.CaridadMichael.ClothingStore.model.user.UserAddress;
 
 @Entity
 public class Product {
@@ -18,6 +23,9 @@ public class Product {
 	private LocalDateTime createdAt;
 	private LocalDateTime modifiedAt;
 	private LocalDateTime deletedAt;
+	
+	
+	
 	
 	
 	
