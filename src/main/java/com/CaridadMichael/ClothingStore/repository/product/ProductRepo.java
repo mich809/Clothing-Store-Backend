1551ba590db62f6286bd7bd491b73e1d1dcb8c10
Michael Caridad <caridadmichael@gmail.com>
2022-12-09 21:05:31 -0400

    rename class

--- src/main/java/com/CaridadMichael/ClothingStore/repository/product/ProductRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/product/ProductRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.CaridadMichael.ClothingStore.model.product.Product;
 
-public interface ProductRepository  extends JpaRepository<Product, Long> {
+public interface ProductRepo  extends JpaRepository<Product, Long> {
 
 }
