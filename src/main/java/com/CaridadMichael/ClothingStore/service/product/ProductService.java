835f49efa53310fb9ce41e5d60d07da1329a98c9
Michael Caridad <caridadmichael@gmail.com>
2022-12-09 21:05:47 -0400

    added temporaray methods

--- src/main/java/com/CaridadMichael/ClothingStore/service/product/ProductService.java
+++ src/main/java/com/CaridadMichael/ClothingStore/service/product/ProductService.java
@@ -1,5 +1,34 @@
 package com.CaridadMichael.ClothingStore.service.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.CaridadMichael.ClothingStore.repository.product.ProductCategoryRepo;
+import com.CaridadMichael.ClothingStore.repository.product.ProductRepo;
+
+@Service
 public class ProductService {
+	@Autowired
+	private ProductRepo productRepo;
+
+	@Autowired
+	private ProductCategoryRepo productCategoryRepo;
+
+	public String createCategory() {
+		return "create";
+	}
+
+	public String getProductCategory() {
+		return "get category";
+	}
+
+	public String getAllProductCategories() {
+		return "get all product category";
+
+	}
+
+	public String deleteCategory() {
+		return "deleted";
+	}
 
 }
