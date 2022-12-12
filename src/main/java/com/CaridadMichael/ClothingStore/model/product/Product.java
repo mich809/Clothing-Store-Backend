f4b52eb01e77922fa9f471d9d13ab2413a4966ca
Michael Caridad <caridadmichael@gmail.com>
2022-12-12 15:05:22 -0400

    add generationType.identity

--- src/main/java/com/CaridadMichael/ClothingStore/model/product/Product.java
+++ src/main/java/com/CaridadMichael/ClothingStore/model/product/Product.java
@@ -1,11 +1,14 @@
 package com.CaridadMichael.ClothingStore.model.product;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Product {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 
 	private String name;
