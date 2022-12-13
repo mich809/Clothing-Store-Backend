81aa8c389adb98a5c8268cb6deadcc0cd5af7827
Michael Caridad <caridadmichael@gmail.com>
2022-12-12 21:16:57 -0400

    change interface to pagingAndSortingRepo

--- src/main/java/com/CaridadMichael/ClothingStore/repository/product/ProductRepo.java
+++ src/main/java/com/CaridadMichael/ClothingStore/repository/product/ProductRepo.java
@@ -1,9 +1,9 @@
 package com.CaridadMichael.ClothingStore.repository.product;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.CaridadMichael.ClothingStore.model.product.Product;
 
-public interface ProductRepo  extends JpaRepository<Product, Long> {
+public interface ProductRepo  extends PagingAndSortingRepository<Product, Long> {
 
 }
